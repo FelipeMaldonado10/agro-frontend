7bf611ec647175ea1b59ad07bfc6226c04eec46a
FelipeMaldonado10 <afmaldonado10@gmail.com>
2025-08-16 20:15:20 -0500

    se añadio environments

--- src/app/user-management/user-management.service.ts
+++ src/app/user-management/user-management.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({ providedIn: 'root' })
 export class UserManagementService {
-  private apiUrl = 'http://localhost:4000/api/users';
-
+  private apiUrl = `${environment.apiUrl}/users`;
   constructor(private http: HttpClient) {}
 
   private getHeaders() {
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://agro-backend-r4sb.onrender.com/api' // Production API URL
+};
